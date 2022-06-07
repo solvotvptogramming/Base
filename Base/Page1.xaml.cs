0a9e324abd28cba5a49460de5a01c5dfcf370f0a
S1mak04 <simak.brt@gmail.com>
2022-06-07 12:55:46 +0800

    One

---
+++ Base/Page1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Base
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var db = new vlad_uEntities();
+            var users = db.Users.Where((user) => user.login == Login.Text && user.password == Parol.Password).ToList();
+            if (users.Count > 0)
+            {
+                NavigationService.Navigate(new Page2());
+            }
+            else
+            {
+                MessageBox.Show("You are not in the list or you entered incorrect data");
+            }
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Registration());
+        }
+    }
+}
---
+++ Base/Registration.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Base
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Page
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Parol1.Password == Parol2.Text || Parol1.Password.Length > 0 && Parol2.Text.Length > 0 && Login1.Text.Length > 0 )
+            {
+                
+                MessageBox.Show("You have successfully registered");
+                NavigationService.Navigate(new Page1());
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+            var db = new vlad_uEntities();
+            var user = new Users();
+            user.login = Login1.Text;
+            user.password = Parol2.Text;
+            db.Users.Add(user);
+            db.SaveChanges();
+
+
+        }
+    }
+}
